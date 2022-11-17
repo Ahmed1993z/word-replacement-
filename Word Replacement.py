4774af44b36f2335062cdc235302d8985fdb654e
Ahmed <57298154+Ahmed1993z@users.noreply.github.com>
2022-11-17 17:15:21 +0100

    first commit

---
+++ Word Replacement.py
@@ -0,0 +1,6 @@
+phrase = input('input your phrase : ')
+print('your phrase is : ' + phrase)
+word1 = input('input the part you want replaced: ')
+word2 = input('input the part you want to replace it with: ')
+
+print(phrase.replace(word1, word2))
